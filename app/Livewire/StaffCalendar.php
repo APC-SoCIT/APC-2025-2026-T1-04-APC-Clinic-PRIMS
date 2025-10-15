be09415cc62c4ef2bcf4a23f0e2f6d2d311e913e
smcatingub <smcatingub@student.apc.edu.ph>
2025-10-15 21:50:02 +0800

    updated doctor category + doctor sched

--- app/Livewire/StaffCalendar.php
+++ app/Livewire/StaffCalendar.php
@@ -36,10 +36,9 @@ class StaffCalendar extends Component
     public $doctors;
     public $selectedDoctor;
     public $timeSlots = [
-        '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM',
-        '10:00 AM', '10:30 AM', '11:00 AM','11:30 AM', '12:00 PM', 
-        '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM',
-        '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM',
+        '2:00 PM', '2:15 PM', '2:30 PM', '2:45 PM',
+        '3:00 PM', '3:15 PM', '3:30 PM', '3:45 PM',
+        '4:00 PM', '4:15 PM', '4:30 PM', '4:45 PM',
     ];
     public $selectedTimes = [];
     public $isEditingSchedule = false;
