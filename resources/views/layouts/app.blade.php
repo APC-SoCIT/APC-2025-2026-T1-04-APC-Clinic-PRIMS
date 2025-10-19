ada53915c70f1ba16488ecce89fff94cf4f0b1b9
smcatingub <smcatingub@student.apc.edu.ph>
2025-10-19 18:06:57 +0800

    removed AI chatbot for patients

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,21 +35,6 @@
         @stack('modals')
 
         @livewireScripts
-
-        @if(auth()->check() && auth()->user()->hasRole('patient'))
-        <script type="text/javascript">
-        (function(d, m){
-            var kommunicateSettings = 
-                {"appId":"3698278920dcab18ffb19053b4df46316","popupWidget":true,"automaticChatOpenOnNavigation":true};
-            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-            window.kommunicate = m; m._globals = kommunicateSettings;
-        })(document, window.kommunicate || {});
-        /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-        </script>
-        @endif
-        
         
     </body>
     <footer class="text-center text-xs text-gray-500 py-2">
